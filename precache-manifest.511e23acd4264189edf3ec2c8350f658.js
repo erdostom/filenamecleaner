249b904e459846557df3bffdbca3d575052d4770
Tamas Erdos <tamas@tamaserdos.com>
2020-02-11 14:42:28 +0200

    Updates

---
+++ precache-manifest.511e23acd4264189edf3ec2c8350f658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d3e6625fc813c7f88d9bbef0189313",
+    "url": "/filenamecleaner/index.html"
+  },
+  {
+    "revision": "98ddb1dcc7d3aa0c5319",
+    "url": "/filenamecleaner/static/css/main.d4143498.chunk.css"
+  },
+  {
+    "revision": "58278d9158b5cbab8625",
+    "url": "/filenamecleaner/static/js/2.19c5f72a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/filenamecleaner/static/js/2.19c5f72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ddb1dcc7d3aa0c5319",
+    "url": "/filenamecleaner/static/js/main.8fcaf0e2.chunk.js"
+  },
+  {
+    "revision": "3aefc49506116c769e05",
+    "url": "/filenamecleaner/static/js/runtime-main.69f8fb2e.js"
+  }
+]);
